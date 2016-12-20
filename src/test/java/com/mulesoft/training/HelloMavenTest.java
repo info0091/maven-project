baf973317190fd73ef15665759a621648c835f89
Gauthier Francis <info0091@braultetmartineau.com>
2016-12-20 12:17:29 -0500

    Removed tests

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -1,4 +1,4 @@
-package com.mulesoft.training;
+/*package com.mulesoft.training;
 
 import static org.junit.Assert.*;
 
@@ -8,21 +8,21 @@ import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
-/*    @Test
+    @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
         runFlowAndExpect("mavenFlow", "Hello Maven");
-    }*/
+    }
     
-/*    @Test
+    @Test
     public void retrieveFlightsAddsAppropriateHeader() throws Exception {
       MuleEvent event = runFlow("retrieveFlights");
       String contentType = event.getMessage().getOutboundProperty("Content-Type");
       assertEquals("application/json", contentType);
-    }*/
+    }
     
     @Override
     protected String getConfigFile() {
         return "maven-project.xml";
     }
 
-}
+}*/
